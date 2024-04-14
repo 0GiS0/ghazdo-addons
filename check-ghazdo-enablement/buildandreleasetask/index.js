2d4ae3883e1d0f4d63a4b49876a327795fae5469
0GiS0 <giselatb@outlook.com>
2024-04-14 23:08:31 +0200

    Update GHAzDO configuration in ghazdo-postjob-decorator.yml

---
+++ check-ghazdo-enablement/buildandreleasetask/index.js
@@ -0,0 +1,42 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const tl = require("azure-pipelines-task-lib");
+const axios_1 = __importDefault(require("axios"));
+async function run() {
+    const area = "Management";
+    const apiVersion = "7.2-preview";
+    let organization = tl
+        .getVariable("System.CollectionUri")
+        ?.replace("https://dev.azure.com/", "")
+        .replace("/", "");
+    let repository = tl.getVariable("Build.Repository.Name");
+    let project = tl.getVariable("System.TeamProject");
+    // Print values
+    console.log(`Repository: ${repository}`);
+    console.log(`Project: ${project}`);
+    console.log(`Organization: ${organization}`);
+    // Get token
+    let token = tl.getVariable("System.AccessToken");
+    // Make a request
+    // GET https://advsec.dev.azure.com/{organization}/{project}/_apis/management/repositories/{repository}/enablement?api-version=7.2-preview.1
+    let url = `https://advsec.dev.azure.com/${organization}/${project}/_apis/${area}/repositories/${repository}/enablement?api-version=${apiVersion}`;
+    console.log(`URL: ${url}`);
+    axios_1.default
+        .get(url, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    })
+        .then((response) => {
+        console.log(response.data);
+        let advSecEnabled = response.data.advSecEnabled;
+        tl.setVariable("advSecEnabled", advSecEnabled);
+    })
+        .catch((error) => {
+        console.error(error);
+    });
+}
+run();
